d99d1153349913822b3b6bcb3cc7e1d22204907d
Francisco Ferreira <estudosfagf@gmail.com>
2025-05-02 16:35:30 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/test', function(){
+    $products = Product::all();
+    echo "<pre>";
+    print_r($products->toArray());
+});
