04a244c3e9dff98a785d2f262079449374d7c7f5
Francisco Ferreira <estudosfagf@gmail.com>
2025-05-11 08:24:19 -0300

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -24,8 +24,39 @@ class MainController extends Controller
         // $result = $this->ArrayOfObject(Product::limit(3)->get()->toArray());
 
         // buscar produto por id
-        $result = $this->ArrayOfObject(Product::find(10)->toArray());
-        $this->showData($result);
+        // $result = $this->ArrayOfObject(Product::find(10)->toArray());
+        // $this->showData($result);
+
+        //usar cláusula where
+        // $result = Product::where('price', '>=', 70)->get()->toArray();
+        // $this->showData($result);
+
+        //buscar apenas o primeiro
+        // $result = Product::where('price', '>=', 70)
+        //                     ->first()
+        //                     ->toArray();
+
+        //buscar apenas o primeiro elemento se ele existir, caso contrario retornar vazio
+        // $result = Product::where('price', '>=', 190)
+        //                    ->firstOr( function(){
+        //                     return [];
+        //                    });
+        // $this->showData($result);
+
+        // $product = Product::find(10);
+        // echo $product->price; // price da db
+        // echo '<br>';
+        
+        // $product->price = 200;
+        // echo $product->price; // definir o novo preço apenas no codigo (não na db)
+        // echo '<br>';
+        
+        // $product->refresh();
+        // echo $product->price; // recupera o valor da db
+        // echo '<br>';
+
+
+
     }
 
     private function showData($data){
