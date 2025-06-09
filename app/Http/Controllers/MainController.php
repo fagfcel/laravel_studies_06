beb025f22dcf709a7fa57926a2e22866291affd2
Francisco Ferreira <estudosfagf@gmail.com>
2025-06-09 16:45:00 -0300

    013

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -130,6 +130,35 @@ class MainController extends Controller
         
     }
 
+    public function RunningQueries(){
+
+        // $client1 = Client::find(1);
+        // $phones = $client1->phones()->where('phone_number', 'like', '8%')->get();
+        // echo 'Cliente: '. $client1->client_name . '<br>';
+        // echo 'Telefones: <br>';
+        // foreach($phones as $phone){
+        //     echo $phone->phone_number . '<br>';
+        // }
+
+        // $client2 = Client::find(1);
+        // $products = $client2->products()->where('price', '>', 50)->orderBy('product_name')->get();
+        // echo 'Cliente: '. $client2->client_name . '<br>';
+        // echo 'Produtos: <br>';
+        // foreach($products as $product){
+        //     echo $product->product_name . "\tprice: R$" . $product->price . '<br>';
+        // }
+
+         $client2 = Client::find(1);
+        $products = $client2->products()->where('price', '>', 50)->distinct()->orderBy('product_name')->get();
+        echo 'Cliente: '. $client2->client_name . '<br>';
+        echo 'Produtos: <br>';
+        foreach($products as $product){
+            echo $product->product_name . "\tprice: R$" . $product->price . '<br>';
+        }
+
+
+    }
+
     private function showData($data){
         echo "<pre>";
         print_r($data);
